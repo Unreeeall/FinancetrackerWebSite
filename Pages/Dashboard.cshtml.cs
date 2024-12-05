8f94be1187ac26592fdda30e8f16651761fe28c3
Unreal <F.Wenningkamp@beckhoff.com>
2024-12-05 15:27:29 +0100

    Added Adding transactions into the Dashboard (To Fix: Contract and Cycle are not beeing transfered correctly) Missing: Speciallized Transactions for crypto and stocks

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -27,6 +27,37 @@ public class DashboardModel : PageModel
     public CurrencyType Currency { get; set; }
 
 
+    [BindProperty]
+    public required string AccID { get; set; }
+
+    [BindProperty]
+    public required string TransactionType { get; set; }
+
+    [BindProperty]
+    public required string Origin { get; set; } = "";
+
+    [BindProperty]
+    public required string Destination { get; set; } = "";
+
+    [BindProperty]
+    public required string Category { get; set; }
+
+    [BindProperty]
+    public required string Description { get; set; }
+
+    [BindProperty]
+    public required decimal Amount { get; set; }
+
+    [BindProperty]
+    public required DateTime Date { get; set; }
+
+    [BindProperty]
+    public bool Contract { get; set; }
+
+    [BindProperty]
+    public BillingCycle Cycle { get; set; }
+
+
 
     public IActionResult OnGet()
     {
@@ -40,7 +71,7 @@ public class DashboardModel : PageModel
     }
 
 
-    public IActionResult OnPost()
+    public IActionResult OnPostAddFinanceAccount()
     {
         try
         {
@@ -51,12 +82,68 @@ public class DashboardModel : PageModel
             if (WebUser == null) return RedirectToPage("/Index");
 
             _sharedServices.AddFinanceAccount(WebUser, AccountType, Currency, AccountName);
-           
+
             return RedirectToPage("/Dashboard");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error adding FinancialAccount: {ex.Message}");
+            return RedirectToPage("/Error"); // Handle error appropriately
+        }
+    }
+
+    public IActionResult OnPostAddTransaction()
+    {
+        try
+        {
+            WebUser = null;
+            if (!Request.Cookies.TryGetValue("SessionCookie", out string? sessionId)) return RedirectToPage("/Index");
+            if (sessionId == null) return RedirectToPage("/Index");
+            WebUser = WebUser.GetUserBySession(sessionId);
+            if (WebUser == null) return RedirectToPage("/Index");
+
+            Console.WriteLine($"AccID: {AccID}");
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine($"TransactionType: {TransactionType}");
+            Console.WriteLine($"Origin: {Origin}");
+            Console.WriteLine($"Destination: {Destination}");
+            Console.WriteLine($"Category: {Category}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Date: {Date}");
+            Console.WriteLine($"Amount: {Amount}");
+            Console.WriteLine($"Contract: {Contract}");
+            Console.WriteLine($"Cycle: {Cycle}");
+
+            var newTransaction = new Transaction
+            (
+                TransactionType,
+                Date,
+                Amount,
+                Origin,
+                Destination,
+                Description,
+                Category,
+                System.Guid.NewGuid().ToString(),
+                WebUser.GetFinancialAccountByID(AccID),
+                Contract,
+                Cycle
+            );
+            WebUser.Transactions.Add(newTransaction);
+
+            return RedirectToPage("/Dashboard");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error adding Transaction: {ex.Message}");
             return RedirectToPage("/Error"); // Handle error appropriately
         }
     }
